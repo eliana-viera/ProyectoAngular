ab1ed0b7f3f19b43191b1f264febfd5a7b8837b5
Eliana Viera <ely.viera01@gmail.com>
2021-06-15 12:59:33 -0300

    Se agregan clases, componentes y servicio

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EmpleadoComponent } from './Componentes/empleado/empleado.component';
+import { ProductoComponent } from './Componentes/producto/producto.component';
+import { ServicioComponent } from './Componentes/servicio/servicio.component';
+import { UsuarioComponent } from './Componentes/usuario/usuario.component';
+import { ProductoService } from './Servicios/producto.service';
+import { EmpleadoService } from './Servicios/empleado.service';
+import { ServicioService } from './Servicios/servicio.service';
+import { UsuarioService } from './Servicios/usuario.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoComponent,
+    ProductoComponent,
+    ServicioComponent,
+    UsuarioComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  ProductoService,
+  EmpleadoService,
+  ServicioService,
+  UsuarioService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
